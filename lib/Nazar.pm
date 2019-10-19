17fe727c5f0950b1f1a63a8b6f576d9955dbd621
Nazar Kondratyuk <muhach@ukr.net>
2019-10-19 13:46:30 +0300

    Improve Client interface

--- lib/Nazar.pm
+++ lib/Nazar.pm
@@ -34,12 +34,19 @@ sub startup {
   $r->get('/track')->to( 'example#read_track' );
   $r->get('/track/create')->to( 'example#create_track' );
   $r->get('/track/delete')->to( 'example#delete_track' );
-
   $r->get('/client')->to( 'client#read_client' );
   $r->get ('/client/edit/<:id>')->to( 'client#edit_form' );
   $r->post('/client/edit/<:id>')->to( 'client#save_form' );
   $r->get('/client/create')->to( 'client#create_client' );
   $r->get('/client/delete/<:id>')->to( 'client#delete_client' );
+
+  $r->get ( '/client'              )->to( 'client#list_clients'  );
+  $r->get ( '/client/create'       )->to( 'client#create_form'  );
+  $r->post( '/client/create'       )->to( 'client#save_form'    );
+  $r->get ( '/client/show/<:id>'   )->to( 'client#show_client'  );
+  $r->get ( '/client/edit/<:id>'   )->to( 'client#edit_form'    );
+  $r->post( '/client/edit/<:id>'   )->to( 'client#update_form'  );
+  $r->get ( '/client/delete/<:id>' )->to( 'client#delete_client');
 
   $r->get('/figura')->to( 'example#read_figura' );
   $r->get('/figura/create')->to( 'example#create_figura' );
--- lib/Nazar/Controller/Client.pm
+++ lib/Nazar/Controller/Client.pm
@@ -1,7 +1,7 @@
 package Nazar::Controller::Client;
 use Mojo::Base 'Mojolicious::Controller';
 
-sub read_client {
+sub list_clients {
 	my( $c ) =  @_;
 
 	my @clients =  $c->db
@@ -11,10 +11,108 @@ sub read_client {
 	my $result =  '';
 	for my $i ( @clients ) {
 		$result .= join ' -- ', $i->id, $i->name;
-		$result .= "<a href='/client/edit/" .$i->id ."'>Edit</a>";
+		$result .= " <a href='/client/edit/" .$i->id ."'>Edit</a>";
+		$result .= " <a href='/client/delete/" .$i->id ."'>Delete</a>";
 		$result .= '<br>';
 	}
 	$c->render( text => $result );
+}
+
+
+
+sub create_form {
+	my( $c ) =  @_;
+
+	my $form =  <<"	TEXT";
+	<form action="" method="POST">
+	<label for="GET-name">Name:</label>
+	<input id="GET-name" type="text" name="name">
+	<input type="submit" value="Save">
+	</form>
+	TEXT
+
+	$c->render( text => $form );
+}
+
+
+
+sub save_form {
+	my( $c ) =  @_;
+
+	my $name =  $c->param( 'name' );
+	# my $name =  $c->param( 'x' );
+	# my $name =  $c->param( 'y' );
+	$c->db->resultset( 'Client' )->create({
+		name => $name,
+		# x    => $x,
+		# y    => $y,
+	});
+
+	$c->render( text => 'Data saved' );
+}
+
+
+
+sub show_client {
+	my ( $c ) = @_;
+
+	my $id =  $c->param( 'id' );
+
+	my $client =  $c->db
+		->resultset( 'Client' )
+		->search({ id => $id });
+	# [ ....... ]
+
+
+	$client =  $client->first;
+	# [0]
+
+	my $result .= join ' -- ', $client->id, $client->name;
+
+	$c->render( text => $result );
+}
+
+
+
+sub edit_form {
+	my( $c ) =  @_;
+
+	my $id =  $c->param( 'id' );
+	my $client =  $c->db
+		->resultset( 'Client' )
+		->search({id => $id,})
+		->first;
+
+	my $name =  $client->name;
+
+	my $form =  <<"	TEXT";
+	<form action="" method="POST">
+	<label for="GET-name">Name:</label>
+	<input id="GET-name" type="text" name="name" value="$name">
+	<input type="submit" value="Save">
+	</form>
+	TEXT
+
+	$c->render( text => $form );
+}
+
+
+
+sub update_form {
+	my( $c ) =  @_;
+
+	my $id =  $c->param( 'id' );
+	my $client =  $c->db->resultset( 'Client' )->search({
+		id => $id,
+	})->first;
+
+	my $name =  $c->param( 'name' );
+
+	$client->update({
+		name => $name,
+	});
+
+	$c->render( text => 'Data updated' );
 }
 
 
@@ -24,11 +122,11 @@ sub delete_client {
 
 	my $id =  $c->param( 'id' );
 
-
 	my $ds =  $c->db->resultset( 'Client' );
 
-	my $client =  $ds->search({ id => [ $id, $id+1 ] });
+	my $client =  $ds->search({ id => [ split ',', $id ] }); #({ id => [ $id, $id+1 ] });
 	$client->delete;
+
 
 
 	$c->render( text => 'OK' );
@@ -36,71 +134,16 @@ sub delete_client {
 
 
 
-
-sub create_client {
-	my( $c ) =  @_;
-
-	my $ds =  $c->db->resultset( 'Client' );
-
-	my $client =  $ds->create({ 
-		name => 'xxxx',
-	});
-
-	my $result =  join ' -- ', $client->id, $client->name;
-
-	$c->render( text => $result );
-}
-
-
-sub edit_form {
-	my( $c ) =  @_;
-
-	my $id =  $c->param( 'id' );
-	my $client =  $c->db->resultset( 'Client' )->search({
-		id => $id,
-	})->first;
-
-	my $name =  $client->name;
-	# my $ddd =  $client->value;
-
-	my $form =  <<"	TEXT";
-	<form action="" method="POST">
-	  <label for="GET-name">Name:</label>
-	  <input id="GET-name" type="text" name="name" value="$name">
-	  <input type="submit" value="Save">
-
-	  <label for="GET-name">ddd:</label>
-	  <input id="GET-name" type="text" name="x" value="$x">
-	  <input type="submit" value="Save">
-
-	  <label for="GET-name">Namddde:</label>
-	  <input id="GET-name" type="text" name="y" value="$y">
-	  <input type="submit" value="Save">
-	</form>
-	TEXT
-
-	$c->render( text => $form );
-}
-
-sub save_form {
-	my( $c ) =  @_;
-
-	my $id =  $c->param( 'id' );
-	my $client =  $c->db->resultset( 'Client' )->search({
-		id => $id,
-	})->first;
-
-	my $name =  $c->param( 'name' );
-	# my $name =  $c->param( 'x' );
-	# my $name =  $c->param( 'y' );
-	$client->update({
-		name => $name,
-		# x    => $x,
-		# y    => $y,
-	});
-
-
-	$c->render( text => 'Data saved' );
-}
-
 1;
+
+__END__
+
+<body>
+<div> .... </div>
+<div> .... </div>
+<div>
+	<p> ... </p>
+	<p> ... </p>
+	<p>skdfjsldfj <img sdfsdfsdf></img> </p>
+</div>
+</body>
